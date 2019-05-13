dbfebec377fa2c7c714c0c3fd4fbd3b3cdb43b1c
Micah <micah.d.lamb@gmail.com>
2019-05-12 22:53:59 -0400

    fix error

--- betabux.py
+++ betabux.py
@@ -144,7 +144,7 @@ def hold_till_crest(coin):
         prices.append(price)
         obs.append(ob)
         ob_amp = ob * np.average([fit.amp for fit in fits]) / price
-        print(col(f"[{', '.join(rnd(w*1e3) for w in waves)}] => {rnd(wave*1e3)}", 24), col(round(ob,1)), col(rnd(ob_amp*1e3,7)), col(percentage(gain)))
+        print(col(f"[{', '.join(rnd(w*1e3) for w in waves)}] => {rnd(wave*1e3)}", 24), col(round(ob,1)), col(rnd(ob_amp*1e3), 7), col(percentage(gain)))
 
         if wave + ob_amp < 0:
             try:
